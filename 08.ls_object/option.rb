db21d92086dd5e4f6bd28b3ba3aae046859fbab4
“sasacoJM” <78665068+Sasaco-JM@users.noreply.github.com>
2022-06-05 16:50:51 +0900

    レビュー指摘対応：Optionsクラスの重複処理を修正

--- 08.ls_object/option.rb
+++ 08.ls_object/option.rb
@@ -20,8 +20,8 @@ class Option
   end
 
   def activate_option(opt)
-    opt.on('-a') { @activated_options[:a] = true } # aオプションを使うと、ファイル名の先頭にピリオドがあるファイルも表示する。
-    opt.on('-l') { @activated_options[:l] = true } # 「l」はlongなフォーマットを意味する。longというだけあって詳細を表示して、横長になる。
-    opt.on('-r') { @activated_options[:r] = true } # 逆順で表示する。
+    %i[a l r].each do |sym|
+      opt.on("-#{sym}") { @activated_options[sym] = true }
+    end
   end
 end
